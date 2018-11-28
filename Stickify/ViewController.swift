8fef195d61434700a5b4804463228293ee8fea16
Dominic Waitforit Drees <dominic.drees@live.de>
2018-11-28 21:02:31 +0100

    Initial Commit

---
+++ Stickify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stickify
+//
+//  Created by Dominic Drees on 28.11.18.
+//  Copyright © 2018 DeltaSiege. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
